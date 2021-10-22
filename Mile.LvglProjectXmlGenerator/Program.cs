04b2ac8b2f97dacca31c738c8c71a080284ba322
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-10-22 23:32:31 +0800

    Add Mile.LvglProjectXmlGenerator project.

---
+++ Mile.LvglProjectXmlGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mile.LvglProjectXmlGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
