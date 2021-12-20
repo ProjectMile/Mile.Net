0b4dcdd689c51448f49edbba6882f577b306245a
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-12-20 22:47:46 +0800

    Update Mile.LvglProjectXmlGenerator.

--- Mile.LvglProjectXmlGenerator/Program.cs
+++ Mile.LvglProjectXmlGenerator/Program.cs
@@ -44,13 +44,7 @@ namespace Mile.LvglProjectXmlGenerator
                 rootPath + @"\lvgl",
                 rootPath + @"\",
                 rootPath,
-                @"LVGL.Core");
-
-            VisualStudioCppItemsProjectGenerator.Generate(
-                rootPath + @"\lv_demos",
-                rootPath + @"\",
-                rootPath,
-                @"LVGL.Demonstrations");
+                @"LVGL.Portable");
         }
 
         static void LvglSimulatorProject()
@@ -62,13 +56,7 @@ namespace Mile.LvglProjectXmlGenerator
                 rootPath + @"\lvgl",
                 rootPath + @"\",
                 rootPath,
-                @"LVGL.Core");
-
-            VisualStudioCppItemsProjectGenerator.Generate(
-                rootPath + @"\lv_demos",
-                rootPath + @"\",
-                rootPath,
-                @"LVGL.Demonstrations");
+                @"LVGL.Portable");
 
             VisualStudioCppItemsProjectGenerator.Generate(
                 rootPath + @"\lv_drivers",
@@ -81,8 +69,8 @@ namespace Mile.LvglProjectXmlGenerator
         static void Main(string[] args)
         {
             //MileUefiProject();
-            MileLilyProject();
-            //LvglWindowsProject();
+            //MileLilyProject();
+            LvglWindowsProject();
             //LvglSimulatorProject();
 
             Console.WriteLine("Hello World!");
