de814856e12266ccc67cfda99d3efc92de741a1e
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-12-30 01:08:21 +0800

    Update Mile.NanaZipProjectXmlGenerator.

--- Mile.NanaZipProjectXmlGenerator/Program.cs
+++ Mile.NanaZipProjectXmlGenerator/Program.cs
@@ -59,6 +59,7 @@ namespace Mile.NanaZipProjectXmlGenerator
                 "tpz",
                 "txz",
                 "vhd",
+                "vhdx",
                 "wim",
                 "xar",
                 "xz",
@@ -185,14 +186,64 @@ namespace Mile.NanaZipProjectXmlGenerator
             }
         }
 
+        static void ConvertFilesToUtf8Bom(
+            string path)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+
+            foreach (var item in folder.GetDirectories())
+            {
+                if (item.Name == "Lang")
+                {
+                    continue;
+                }
+
+                ConvertFilesToUtf8Bom(item.FullName);
+            }
+
+            foreach (var item in folder.GetFiles())
+            {
+                if (item.Extension == ".c" ||
+                    item.Extension == ".c++" ||
+                    item.Extension == ".cc" ||
+                    item.Extension == ".cpp" ||
+                    item.Extension == ".cxx" ||
+                    item.Extension == ".h" ||
+                    item.Extension == ".h++" ||
+                    item.Extension == ".hh" ||
+                    item.Extension == ".hpp" ||
+                    item.Extension == ".hxx" ||
+                    item.Extension == ".idl" ||
+                    item.Extension == ".inl" ||
+                    item.Extension == ".ipp" ||
+                    item.Extension == ".tlh" ||
+                    item.Extension == ".tli" ||
+                    (item.Extension != ".rc" &&
+                    item.Extension != ".sh" &&
+                    item.Extension != ".asm"))
+                {
+                    File.WriteAllText(
+                        item.FullName,
+                        File.ReadAllText(item.FullName, Encoding.UTF8),
+                        Encoding.UTF8);
+                }
+                else
+                {
+                    Console.WriteLine(item.FullName);
+                }
+            }
+        }
+
         public static void Main(string[] args)
         {
             //GenerateSharedSevenZipZStandardProject();
 
             //string Result = GenerateArchiveTypesManifestDefinitions();
 
-            SwitchToPreview();
+            //SwitchToPreview();
             //SwitchToRelease();
+
+            ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\SevenZip");
 
             Console.WriteLine("Hello World!");
 
