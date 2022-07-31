f59a79c153b554f98b3503ccb603547463e87f3f
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-07-31 12:59:30 +0800

    Add Mile.ConvertFilesToUtf8Bom.

---
+++ Mile.ConvertFilesToUtf8Bom/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace Mile.ConvertFilesToUtf8Bom
+{
+    internal class Program
+    {
+        static void ConvertFilesToUtf8Bom(
+            string path)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+
+            foreach (var item in folder.GetDirectories())
+            {
+                if (item.Name == "Lang")
+                {
+                    continue;
+                }
+
+                ConvertFilesToUtf8Bom(item.FullName);
+            }
+
+            foreach (var item in folder.GetFiles())
+            {
+                if (item.Extension == ".c" ||
+                    item.Extension == ".c++" ||
+                    item.Extension == ".cc" ||
+                    item.Extension == ".cpp" ||
+                    item.Extension == ".cxx" ||
+                    item.Extension == ".h" ||
+                    item.Extension == ".h++" ||
+                    item.Extension == ".hh" ||
+                    item.Extension == ".hpp" ||
+                    item.Extension == ".hxx" ||
+                    item.Extension == ".idl" ||
+                    item.Extension == ".inl" ||
+                    item.Extension == ".ipp" ||
+                    item.Extension == ".tlh" ||
+                    item.Extension == ".tli" ||
+                    (item.Extension != ".rc" &&
+                    item.Extension != ".sh" &&
+                    item.Extension != ".asm"))
+                {
+                    try
+                    {
+                        File.WriteAllText(
+                            item.FullName,
+                            File.ReadAllText(item.FullName, Encoding.UTF8),
+                            Encoding.UTF8);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(
+                            "{0}: {1}",
+                            item.FullName,
+                            ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(item.FullName);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\SevenZip");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\BLAKE3");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\Brotli");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\Lizard");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\LZ4");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\LZ5");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\RHash");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\xxHash");
+            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\Zstandard");
+
+            ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaRun\ThirdParty\Detours");
+
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
--- Mile.NanaZipProjectXmlGenerator/Program.cs
+++ Mile.NanaZipProjectXmlGenerator/Program.cs
@@ -292,64 +292,6 @@ namespace Mile.NanaZipProjectXmlGenerator
             }
         }
 
-        static void ConvertFilesToUtf8Bom(
-            string path)
-        {
-            DirectoryInfo folder = new DirectoryInfo(path);
-
-            foreach (var item in folder.GetDirectories())
-            {
-                if (item.Name == "Lang")
-                {
-                    continue;
-                }
-
-                ConvertFilesToUtf8Bom(item.FullName);
-            }
-
-            foreach (var item in folder.GetFiles())
-            {
-                if (item.Extension == ".c" ||
-                    item.Extension == ".c++" ||
-                    item.Extension == ".cc" ||
-                    item.Extension == ".cpp" ||
-                    item.Extension == ".cxx" ||
-                    item.Extension == ".h" ||
-                    item.Extension == ".h++" ||
-                    item.Extension == ".hh" ||
-                    item.Extension == ".hpp" ||
-                    item.Extension == ".hxx" ||
-                    item.Extension == ".idl" ||
-                    item.Extension == ".inl" ||
-                    item.Extension == ".ipp" ||
-                    item.Extension == ".tlh" ||
-                    item.Extension == ".tli" ||
-                    (item.Extension != ".rc" &&
-                    item.Extension != ".sh" &&
-                    item.Extension != ".asm"))
-                {
-                    try
-                    {
-                        File.WriteAllText(
-                            item.FullName,
-                            File.ReadAllText(item.FullName, Encoding.UTF8),
-                            Encoding.UTF8);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(
-                            "{0}: {1}",
-                            item.FullName,
-                            ex.Message);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(item.FullName);
-                }
-            }
-        }
-
         static SortedDictionary<int, string> ParseSevenZipLanguageFile(
             string path)
         {
@@ -691,16 +633,6 @@ namespace Mile.NanaZipProjectXmlGenerator
 
             //SwitchToPreview();
             //SwitchToRelease();
-
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\SevenZip");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\BLAKE3");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\Brotli");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\Lizard");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\LZ4");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\LZ5");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\RHash");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\xxHash");
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\ThirdParty\Zstandard");
 
             //string Result = ConvertSevenZipLanguageFilesToModernResources();
             //ConvertSevenZipLanguageFilesToSfxSelfContain();
