28c4c425d2c4160f38725d52797368da4a871854
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-10-27 23:37:22 +0800

    Update Mile.NanaBoxProjectXmlGenerator.

--- Mile.NanaBoxProjectXmlGenerator/Program.cs
+++ Mile.NanaBoxProjectXmlGenerator/Program.cs
@@ -58,10 +58,41 @@ namespace Mile.NanaBoxProjectXmlGenerator
             }
         }
 
+        static string GeneratePackageManifestResourceIdentities()
+        {
+            string Result = "";
+
+            DirectoryInfo folder = new DirectoryInfo(
+                @"D:\Projects\MouriNaruto\NanaBox\NanaBox\Strings");
+
+            foreach (var item in folder.GetDirectories())
+            {
+                Result += string.Format(
+                    "    <Resource Language=\"{0}\" />\r\n",
+                    item.Name);
+            }
+
+            int[] Scales = new int[]
+            {
+                100, 125, 150, 200, 400
+            };
+
+            foreach (var scale in Scales)
+            {
+                Result += string.Format(
+                    "    <Resource uap:Scale=\"{0}\" />\r\n",
+                    scale.ToString());
+            }
+
+            return Result;
+        }
+
         static void Main(string[] args)
         {
-            SwitchToPreview();
+            //SwitchToPreview();
             //SwitchToRelease();
+
+            string Result = GeneratePackageManifestResourceIdentities();
 
             Console.WriteLine("Hello, World!");
 
