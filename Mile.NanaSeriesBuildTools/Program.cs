873c055ca218306dbcb40256a3b4aa4b8e03718b
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-24 00:19:24 +0800

    Create Mile.NanaSeriesBuildTools project.

---
+++ Mile.NanaSeriesBuildTools/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Mile.NanaSeriesBuildTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
