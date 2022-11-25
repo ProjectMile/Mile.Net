13ee1a341c17de654b721f864a3e99c221a1f8bc
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-25 23:43:25 +0800

    Update Mile.ModernProjectAssetsGenerator.

--- Mile.ModernProjectAssetsGenerator/Program.cs
+++ Mile.ModernProjectAssetsGenerator/Program.cs
@@ -5,36 +5,6 @@ namespace Mile.ModernProjectAssetsGenerator
 {
     class Program
     {
-        static void NanaZipProject()
-        {
-            string sourceRoot = @"D:\Projects\MouriNaruto\NanaZip\Assets\";
-            string outputPath = @"D:\Projects\MouriNaruto\NanaZip\Assets\PackageAssets";
-
-            MsixPackageAssetsGenerator.GenerateApplicationImageAssets(
-                sourceRoot + @"NanaZip_HighResolution_ContrastStandard.png",
-                sourceRoot + @"NanaZip_HighResolution_ContrastBlack.png",
-                sourceRoot + @"NanaZip_HighResolution_ContrastWhite.png",
-                outputPath);
-
-            MsixPackageAssetsGenerator.GenerateFileAssociationImageAssets(
-                sourceRoot + @"NanaZip_HighResolution_ArchiveFile.png",
-                outputPath,
-                @"ArchiveFile");
-
-            outputPath = @"D:\Projects\MouriNaruto\NanaZip\Assets\PreviewPackageAssets";
-
-            MsixPackageAssetsGenerator.GenerateApplicationImageAssets(
-                sourceRoot + @"NanaZipPreview_HighResolution_ContrastStandard.png",
-                sourceRoot + @"NanaZipPreview_HighResolution_ContrastBlack.png",
-                sourceRoot + @"NanaZipPreview_HighResolution_ContrastWhite.png",
-                outputPath);
-
-            MsixPackageAssetsGenerator.GenerateFileAssociationImageAssets(
-                sourceRoot + @"NanaZipPreview_HighResolution_ArchiveFile.png",
-                outputPath,
-                @"ArchiveFile");
-        }
-
         static void NanaGetProject()
         {
             string sourceRoot = @"D:\Projects\MouriNaruto\NanaGet\Assets\";
@@ -97,7 +67,6 @@ namespace Mile.ModernProjectAssetsGenerator
 
         static void Main(string[] args)
         {
-            //NanaZipProject();
             NanaGetProject();
             //NanaBoxProject();
 
