2b8c69fea16f7af7bb5c78685c4567a8c4181eaf
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-26 20:50:32 +0800

    Update Mile.ModernProjectAssetsGenerator.

--- Mile.ModernProjectAssetsGenerator/Program.cs
+++ Mile.ModernProjectAssetsGenerator/Program.cs
@@ -5,40 +5,8 @@ namespace Mile.ModernProjectAssetsGenerator
 {
     class Program
     {
-        static void NanaGetProject()
-        {
-            string sourceRoot = @"D:\Projects\MouriNaruto\NanaGet\Assets\";
-            string outputPath = @"D:\Projects\MouriNaruto\NanaGet\Assets\PackageAssets";
-
-            MsixPackageAssetsGenerator.GenerateApplicationImageAssets(
-                sourceRoot + @"NanaGet_HighResolution_ContrastStandard.png",
-                sourceRoot + @"NanaGet_HighResolution_ContrastBlack.png",
-                sourceRoot + @"NanaGet_HighResolution_ContrastWhite.png",
-                outputPath);
-
-            MsixPackageAssetsGenerator.GenerateFileAssociationImageAssets(
-                sourceRoot + @"NanaGet_HighResolution_MetadataFile.png",
-                outputPath,
-                @"MetadataFile");
-
-            outputPath = @"D:\Projects\MouriNaruto\NanaGet\Assets\PreviewPackageAssets";
-
-            MsixPackageAssetsGenerator.GenerateApplicationImageAssets(
-                sourceRoot + @"NanaGetPreview_HighResolution_ContrastStandard.png",
-                sourceRoot + @"NanaGetPreview_HighResolution_ContrastBlack.png",
-                sourceRoot + @"NanaGetPreview_HighResolution_ContrastWhite.png",
-                outputPath);
-
-            MsixPackageAssetsGenerator.GenerateFileAssociationImageAssets(
-                sourceRoot + @"NanaGetPreview_HighResolution_MetadataFile.png",
-                outputPath,
-                @"MetadataFile");
-        }
-
         static void Main(string[] args)
         {
-            NanaGetProject();
-
             Console.WriteLine("Hello World!");
 
             Console.ReadKey();
