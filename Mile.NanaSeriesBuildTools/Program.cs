4ce8d4c910bf896bc3682a56f8de0491779d4df4
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-26 20:48:36 +0800

    Implement Mile.NanaSeriesBuildTools.ProjectAssetsBuildTools.GenerateNanaGetAssets method.

--- Mile.NanaSeriesBuildTools/Program.cs
+++ Mile.NanaSeriesBuildTools/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //ProjectAssetsBuildTools.GenerateNanaZipAssets();
-            ProjectAssetsBuildTools.GenerateNanaBoxAssets();
+            //ProjectAssetsBuildTools.GenerateNanaBoxAssets();
+            ProjectAssetsBuildTools.GenerateNanaGetAssets();
 
             Console.WriteLine("Hello, World!");
 
--- Mile.NanaSeriesBuildTools/ProjectAssetsBuildTools.cs
+++ Mile.NanaSeriesBuildTools/ProjectAssetsBuildTools.cs
@@ -272,5 +272,126 @@ namespace Mile.NanaSeriesBuildTools
                     OutputPath + @"\..\NanaBoxPreview.png");
             }
         }
+
+        public static void GenerateNanaGetAssets()
+        {
+            {
+                string SourcePath = @"D:\Projects\MouriNaruto\NanaGet\Assets\OriginalAssets\NanaGet";
+
+                string OutputPath = @"D:\Projects\MouriNaruto\NanaGet\Assets\PackageAssets";
+
+                ConcurrentDictionary<int, MagickImage> StandardSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+                ConcurrentDictionary<int, MagickImage> ContrastBlackSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+                ConcurrentDictionary<int, MagickImage> ContrastWhiteSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+
+                ConcurrentDictionary<int, MagickImage> MetadataFileSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+
+                foreach (var AssetSize in AssetsUtils.AssetSizes)
+                {
+                    StandardSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "Standard",
+                        AssetSize));
+                    ContrastBlackSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "ContrastBlack",
+                        AssetSize));
+                    ContrastWhiteSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "ContrastWhite",
+                        AssetSize));
+
+                    MetadataFileSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "MetadataFile",
+                        AssetSize));
+                }
+
+                AssetsGenerator.GeneratePackageApplicationImageAssets(
+                    StandardSources,
+                    ContrastBlackSources,
+                    ContrastWhiteSources,
+                    OutputPath);
+
+                AssetsGenerator.GeneratePackageFileAssociationImageAssets(
+                    MetadataFileSources,
+                    OutputPath,
+                    @"MetadataFile");
+
+                AssetsGenerator.GenerateIconFile(
+                    StandardSources,
+                    OutputPath + @"\..\NanaGet.ico");
+
+                StandardSources[64].Write(
+                    OutputPath + @"\..\NanaGet.png");
+            }
+
+            {
+                string SourcePath = @"D:\Projects\MouriNaruto\NanaGet\Assets\OriginalAssets\NanaGetPreview";
+
+                string OutputPath = @"D:\Projects\MouriNaruto\NanaGet\Assets\PreviewPackageAssets";
+
+                ConcurrentDictionary<int, MagickImage> StandardSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+                ConcurrentDictionary<int, MagickImage> ContrastBlackSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+                ConcurrentDictionary<int, MagickImage> ContrastWhiteSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+
+                ConcurrentDictionary<int, MagickImage> MetadataFileSources =
+                    new ConcurrentDictionary<int, MagickImage>();
+
+                foreach (var AssetSize in AssetsUtils.AssetSizes)
+                {
+                    StandardSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "Standard",
+                        AssetSize));
+                    ContrastBlackSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "ContrastBlack",
+                        AssetSize));
+                    ContrastWhiteSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "ContrastWhite",
+                        AssetSize));
+
+                    MetadataFileSources[AssetSize] = new MagickImage(string.Format(
+                        @"{0}\{1}\{1}_{2}.png",
+                        SourcePath,
+                        "MetadataFile",
+                        AssetSize));
+                }
+
+                AssetsGenerator.GeneratePackageApplicationImageAssets(
+                    StandardSources,
+                    ContrastBlackSources,
+                    ContrastWhiteSources,
+                    OutputPath);
+
+                AssetsGenerator.GeneratePackageFileAssociationImageAssets(
+                    MetadataFileSources,
+                    OutputPath,
+                    @"MetadataFile");
+
+                AssetsGenerator.GenerateIconFile(
+                    StandardSources,
+                    OutputPath + @"\..\NanaGetPreview.ico");
+
+                StandardSources[64].Write(
+                    OutputPath + @"\..\NanaGetPreview.png");
+            }
+        }
     }
 }
