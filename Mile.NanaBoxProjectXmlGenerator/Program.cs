81b00a59e69a072610877f23f7a461b3bbcb1522
MouriNaruto <KurikoMouri@outlook.jp>
2023-07-16 03:46:21 +0800

    Remove development channel switch support in Mile.NanaBoxProjectXmlGenerator because the implementation is move to NanaBox project.

--- Mile.NanaBoxProjectXmlGenerator/Program.cs
+++ Mile.NanaBoxProjectXmlGenerator/Program.cs
@@ -4,60 +4,6 @@ namespace Mile.NanaBoxProjectXmlGenerator
 {
     internal class Program
     {
-        static void SwitchToPreview()
-        {
-            string NanaBoxSourceRoot = @"D:\Projects\MouriNaruto\NanaBox";
-
-            {
-                string Path = string.Format(
-                    @"{0}\NanaBoxPackage\Package.appxmanifest",
-                    NanaBoxSourceRoot);
-                string Content = File.ReadAllText(
-                    Path,
-                    Encoding.UTF8);
-                Content = Content.Replace(
-                    "DisplayName=\"NanaBox\"",
-                    "DisplayName=\"NanaBox Preview\"");
-                Content = Content.Replace(
-                    "Name=\"40174MouriNaruto.NanaBox\"",
-                    "Name=\"40174MouriNaruto.NanaBoxPreview\"");
-                Content = Content.Replace(
-                    "<DisplayName>NanaBox</DisplayName>",
-                    "<DisplayName>NanaBox Preview</DisplayName>");
-                File.WriteAllText(
-                    Path,
-                    Content,
-                    Encoding.UTF8);
-            }
-        }
-
-        static void SwitchToRelease()
-        {
-            string NanaBoxSourceRoot = @"D:\Projects\MouriNaruto\NanaBox";
-
-            {
-                string Path = string.Format(
-                    @"{0}\NanaBoxPackage\Package.appxmanifest",
-                    NanaBoxSourceRoot);
-                string Content = File.ReadAllText(
-                    Path,
-                    Encoding.UTF8);
-                Content = Content.Replace(
-                    "DisplayName=\"NanaBox Preview\"",
-                    "DisplayName=\"NanaBox\"");
-                Content = Content.Replace(
-                    "Name=\"40174MouriNaruto.NanaBoxPreview\"",
-                    "Name=\"40174MouriNaruto.NanaBox\"");
-                Content = Content.Replace(
-                    "<DisplayName>NanaBox Preview</DisplayName>",
-                    "<DisplayName>NanaBox</DisplayName>");
-                File.WriteAllText(
-                    Path,
-                    Content,
-                    Encoding.UTF8);
-            }
-        }
-
         static string GeneratePackageManifestResourceIdentities()
         {
             string Result = "";
@@ -89,9 +35,6 @@ namespace Mile.NanaBoxProjectXmlGenerator
 
         static void Main(string[] args)
         {
-            SwitchToPreview();
-            //SwitchToRelease();
-
             //string Result = GeneratePackageManifestResourceIdentities();
 
             Console.WriteLine("Hello, World!");
