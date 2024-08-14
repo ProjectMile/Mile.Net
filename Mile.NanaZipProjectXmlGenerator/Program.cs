731d9062d79f4fa2fa3592bff1c250d3f0caa78d
MouriNaruto <Kenji.Mouri@outlook.com>
2024-08-15 00:24:39 +0800

    Remove GeneratePackageManifestResourceIdentitiesin Mile.NanaZipProjectXmlGenerator because the implementation is move to NanaZip project.

--- Mile.NanaZipProjectXmlGenerator/Program.cs
+++ Mile.NanaZipProjectXmlGenerator/Program.cs
@@ -425,43 +425,12 @@ namespace Mile.NanaZipProjectXmlGenerator
                 Encoding.Unicode);
         }
 
-
-        static string GeneratePackageManifestResourceIdentities()
-        {
-            string Result = "";
-
-            DirectoryInfo folder = new DirectoryInfo(
-                @"D:\Projects\MouriNaruto\NanaZip\NanaZipPackage\Strings");
-
-            foreach (var item in folder.GetDirectories())
-            {
-                Result += string.Format(
-                    "    <Resource Language=\"{0}\" />\r\n",
-                    item.Name);
-            }
-
-            int[] Scales = new int[]
-            {
-                100, 125, 150, 200, 400
-            };
-
-            foreach (var scale in Scales)
-            {
-                Result += string.Format(
-                    "    <Resource uap:Scale=\"{0}\" />\r\n",
-                    scale.ToString());
-            }
-
-            return Result;
-        }
         public static void Main(string[] args)
         {
             //string Result = GenerateArchiveTypesManifestDefinitions();
 
             //string Result = ConvertSevenZipLanguageFilesToModernResources();
             //ConvertSevenZipLanguageFilesToSfxSelfContain();
-
-            //string Result = GeneratePackageManifestResourceIdentities();
 
             Console.WriteLine("Hello World!");
 
