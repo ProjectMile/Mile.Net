30f07595e87302abb0af790539a586972ef62f39
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-07 21:42:04 +0800

    Create Mile.Svg2Ico project.

---
+++ Mile.Svg2Ico/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Mile.Svg2Ico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+               "Mile.Svg2Ico task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
