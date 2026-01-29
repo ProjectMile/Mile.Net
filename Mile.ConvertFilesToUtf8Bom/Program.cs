5d720461682799a3126fa679635b6f5a322e7984
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-30 00:15:22 +0800

    Update Mile.ConvertFilesToUtf8Bom.

--- Mile.ConvertFilesToUtf8Bom/Program.cs
+++ Mile.ConvertFilesToUtf8Bom/Program.cs
@@ -192,13 +192,78 @@ namespace Mile.ConvertFilesToUtf8Bom
             }
         }
 
+        static void ConvertFilesToUtf8(
+           string path)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+
+            foreach (var item in folder.GetDirectories())
+            {
+                if (item.Name == "Lang")
+                {
+                    continue;
+                }
+
+                ConvertFilesToUtf8(item.FullName);
+            }
+
+            foreach (var item in folder.GetFiles())
+            {
+                if (item.Extension == ".c" ||
+                    item.Extension == ".c++" ||
+                    item.Extension == ".cc" ||
+                    item.Extension == ".cpp" ||
+                    item.Extension == ".cxx" ||
+                    item.Extension == ".h" ||
+                    item.Extension == ".h++" ||
+                    item.Extension == ".hh" ||
+                    item.Extension == ".hpp" ||
+                    item.Extension == ".hxx" ||
+                    item.Extension == ".idl" ||
+                    item.Extension == ".inl" ||
+                    item.Extension == ".ipp" ||
+                    item.Extension == ".i" ||
+                    item.Extension == ".tlh" ||
+                    item.Extension == ".tli" ||
+                    item.Extension == ".xaml" ||
+                    item.Extension == ".resw" ||
+                    item.Extension == ".manifest" ||
+                    item.Extension == ".props" ||
+                    item.Extension == ".targets" ||
+                    item.Extension == ".md")
+                {
+                    try
+                    {
+                        File.WriteAllText(
+                            item.FullName,
+                            File.ReadAllText(
+                                item.FullName,
+                                Encoding.UTF8).ReplaceLineEndings("\r\n"),
+                            new UTF8Encoding(false));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(
+                            "{0}: {1}",
+                            item.FullName,
+                            ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(item.FullName);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             //ConvertFilesToUtf8Bom(@"D:\Projects\ProjectMile\Mile.Aria2\Mile.Aria2.Library");
 
-            //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\NanaZip.Codecs");
+            ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\NanaZip.Codecs");
             //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\NanaZip.Core");
             //ConvertFilesToUtf8Bom(@"D:\Projects\MouriNaruto\NanaZip\NanaZip.Universal");
+            //ConvertFilesToUtf8(@"D:\Projects\MouriNaruto\SevenZipMainline");
 
             //ConvertFilesToUtf8Bom(@"D:\Projects\ProjectMile\Mile.Windows.Internal\Mile.Internal\Mile.Internal.Implementation");
 
